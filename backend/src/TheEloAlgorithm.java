39473e2639ab39542b287ffc86a56f96b11e0f67
BratyshevTD <bratyshev2013@mail.ru>
2023-05-15 15:42:18 +0300

    Реализовано вычисление рейтинга Эло

---
+++ backend/src/TheEloAlgorithm.java
@@ -0,0 +1,23 @@
+public class TheEloAlgorithm {
+    private double expectedNumber; // ожидаемое количество очков, которое наберёт игрок
+    private double resultNumber; // исход: 1 - победа, 0.5 - ничья, 0 - поражение
+    private int ratingA; // рейтинг игрока А
+    private int ratingB; // рейтинг игрока B
+
+    private static double getExpectedNumber(int ratingA, int ratingB) {
+        return 1 / (1 + 10 * (ratingB - ratingA) / 400);
+    }
+
+    private static int getNewRating(int ratingA, int resultNumber, int expectedNumber, int fightCount) {
+        int k; // коэффициент, зависящий от рейтинга игрока
+
+        if (ratingA >= 2400) {
+            k = 10;
+        } else if (ratingA <  2400 && fightCount > 3) {
+            k = 20;
+        } else {
+            k = 40;
+        }
+        return ratingA + k * (resultNumber - expectedNumber);
+    }
+}
