51a2ed7f3b3066f3a6f8f9802bef24f89a027647
Xopek <alexxxhrn@gmail.com>
2023-06-17 23:05:00 +0300

    жду json

--- frontend/src/components/FightersTable.vue
+++ frontend/src/components/FightersTable.vue
@@ -13,18 +13,18 @@ export default {
     <tr>
         <th>Rank</th>
         <th>Name</th>
-        <th>Country</th>
+        <!-- <th>Country</th> -->
         <th>Number of Fights</th>
-        <th>Age</th>
-        <th>weightClass</th>
+        <!-- <th>Age</th> -->
+        <th>Weight Class</th>
         <th>Elo Ranking</th>
     </tr>
     <tr v-for="(row, index) in items">
         <td>{{ index + 1 }}</td>
         <td>{{ row.name }}</td>
-        <td>{{ row.country}}</td>
+        <!-- <td>{{ row.country}}</td> -->
         <td>{{ row.fightsNum }}</td>
-        <td>{{ row.age }}</td>
+        <!-- <td>{{ row.age }}</td> -->
         <td>{{ row.weightClass }}</td>
         <td>{{ row.elo }}</td>
     </tr>
--- frontend/src/views/HomeView.vue
+++ frontend/src/views/HomeView.vue
@@ -1,5 +1,8 @@
 <script>
 import FightersTable from '../components/FightersTable.vue';
+import fighters from '../components/fighters.json';
+
+
 
 
 export default {
@@ -7,75 +10,81 @@ export default {
   data() {
     return {
       filterParams: {
-        country: '',
+        // country: '',
         weightClass: ''
       },
-      fighters: [
-        {
-          id: 1,
-          name: 'Konnor',
-          age: '34',
-          country: 'Ireland',
-          fightsNum: 56,
-          elo: 1583,
-          weightClass: 'Lightweight',
-          fightStory: [
-            {
-              opponent: { id: 2, name: 'Khabib' },
-              result: 'win',
-              reason: 'tko'
-            },
-            {
-              opponent: { id: 2, name: 'Khabib' },
-              result: 'win',
-              reason: 'tko'
-            }
-          ],
-        },
 
-        {
-          id: 3,
-          name: 'Konnor',
-          age: '35',
-          country: 'USA',
-          fightsNum: 56,
-          elo: 1583,
-          weightClass: 'Welterweight',
-          fightStory: [
-            {
-              opponent: { id: 2, name: 'Khabib' },
-              result: 'win',
-              reason: 'tko'
-            },
-            {
-              opponent: { id: 2, name: 'Khabib' },
-              result: 'win',
-              reason: 'tko'
-            }
-          ],
-        }
-      ]
+      fighters
+    
+      //   {
+      //     id: 1,
+      //     name: 'Konnor',
+      //     age: '34',
+      //     country: 'Ireland',
+      //     fightsNum: 56,
+      //     elo: 1583,
+      //     weightClass: 'Lightweight',
+      //     fightStory: [
+      //       {
+      //         opponent: { id: 2, name: 'Khabib' },
+      //         result: 'win',
+      //         reason: 'tko'
+      //       },
+      //       {
+      //         opponent: { id: 2, name: 'Khabib' },
+      //         result: 'win',
+      //         reason: 'tko'
+      //       }
+      //     ],
+      //   },
+
+      //   {
+      //     id: 3,
+      //     name: 'Konnor',
+      //     age: '35',
+      //     country: 'USA',
+      //     fightsNum: 56,
+      //     elo: 1583,
+      //     weightClass: 'Welterweight',
+      //     fightStory: [
+      //       {
+      //         opponent: { id: 2, name: 'Khabib' },
+      //         result: 'win',
+      //         reason: 'tko'
+      //       },
+      //       {
+      //         opponent: { id: 2, name: 'Khabib' },
+      //         result: 'win',
+      //         reason: 'tko'
+      //       }
+      //     ],
+      //   }
+      //]
     }
   },
 
+  
   computed: {
     filteredFighters() {
       return this.fighters.filter(x => {
-        return (!this.filterParams.country.length || x.country === this.filterParams.country) &&
-          (!this.filterParams.weightClass.length || x.weightClass === this.filterParams.weightClass)
+        return (!this.filterParams.weightClass.length || x.weightClass === this.filterParams.weightClass)
       })
     },
 
+    
     sortedAndFilteredFighters() {
-      return this.filteredFighters.sort()
+      return this.filteredFighters.sort((a, b) => a.elo - b.elo)
     }
   },
 
-  async created() {
-    // Sending request
-    // const result = await fetch('myapi.com/data')
-    // this.fighters = result.data
-  }
+  // async created() {
+  //   // Sending request
+  //   const fs = require('fs');
+  //   const path = require('path');
+  //   const result = require('../components/fighters.json')
+  //   this.fighters = result.data()
+
+  // }
 }
 
 
@@ -111,35 +120,48 @@ export default {
   <!-- фильтр -->
   <div id="filter-container">
     <div class="filters">
-      <div class="filt">
+      <!-- <div class="filt">
         <label for="country-filter">Country:</label>
         <select id="country-filter" v-model="filterParams.country">
             <option value="">All</option>
             <option value="Ireland">Ireland</option>
             <option value="USA">USA</option>
-            <!-- Add more options for other countries -->
+            
         </select>
-      </div>
+      </div> -->
       
       <div class="filt">
-        <label for="weight-filter">Weight Class:</label>
+        <label class="wfiltText" for="weight-filter">Weight Class:</label>
         <select id="weight-filter" v-model="filterParams.weightClass">
             <option value="">All</option>
+            <option value="Open Weight">Open Weight</option>
             <option value="Lightweight">Lightweight</option>
+            <option value="Heavyweight">Heavyweight</option>
+            <option value="Middleweight">Middleweight</option>
             <option value="Welterweight">Welterweight</option>
+            <option value="Super Heavyweight">Super Heavyweight</option>
+            <option value="Light Heavyweight">Light Heavyweight</option>
+            <option value="Catch Weight">Catch Weight</option>
+            <option value="Featherweight">Featherweight</option>
+            <option value="Bantamweight">Bantamweight</option>
+            <option value="Flyweight">Flyweight</option>
+            <option value="Women's Bantamweight">Women's Bantamweight</option>
+            <option value="Women's Strawweight">Women's Strawweight</option>
+            <option value="Women's Flyweight">Women's Flyweight</option>
+            <option value="Women's Featherweight">Women's Featherweight</option>
             <!-- Add more options for other weight classes -->
         </select>
       </div>
       
-      <div class="filt">
+      <!-- <div class="filt">
         <label for="rating-filter">Rating:</label>
         <input type="number" id="rating-filter" onchange="filterTable()" min="0" max="5000">
-      </div>
+      </div> -->
     </div>
   </div>
     
     
   
-  <FightersTable :items="filteredFighters">
+  <FightersTable :items="sortedAndFilteredFighters">
   </FightersTable>
 </template>
